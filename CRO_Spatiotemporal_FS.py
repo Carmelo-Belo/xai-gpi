b06982542049e263fe7edcb6cd119ee613a783ec
Filippo Dainelli <dainelli.filippo@gmail.com>
2024-08-01 17:05:45 +0200

    init FS adaptation, problems on lag definition -> NaN values

--- CRO_Spatiotemporal_FS.py
+++ CRO_Spatiotemporal_FS.py
@@ -4,58 +4,53 @@ from PyCROSL.SubstrateReal import *
 from PyCROSL.SubstrateInt import *
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, f1_score
+from sklearn import preprocessing
+from sklearn.metrics import f1_score, mean_absolute_error
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
 import warnings
 warnings.filterwarnings('ignore')
 import pandas as pd
 import numpy as np
 import os
 
-
-
-
-"""
-File names to store the solutions provided by the algorithm
-"""
-
-filename = 'Test_Paper_1'
-path_output = './Results/Test_Paper/'
-# Create directory
-if not os.path.exists(path_output):
-    os.makedirs(path_output)
+# Set directories
+project_dir = '/Users/huripari/Documents/PhD/TCs_Genesis'
+fs_dir = os.path.join(project_dir, 'FS_TCG')
+data_dir = os.path.join(fs_dir, 'data')
 
 """
 Path and name of the predictor dataset and target dataset
 """
-path_input = './Data/Paper/'
-predictor_file = 'predictors_dataset.csv'
-target_file = 'target.csv'
+predictor_file = 'predictors_1980-2021_12clusters_4vars_10idxs.csv'
+predictors_path = os.path.join(data_dir, predictor_file)
+target_file = 'target_1980-2021_2.5x2.5.csv'
+target_path = os.path.join(data_dir, target_file)
 
+# Load the dataset and target
+predictors_df = pd.read_csv(predictors_path, index_col=0)
+predictors_df.index = pd.to_datetime(predictors_df.index)
+target_df = pd.read_csv(target_path, index_col=0)
+target_df.index = pd.to_datetime(target_df.index)
 
-
-# Load the dataset
-pred_dataframe = pd.read_csv(path_input+predictor_file, index_col=0)
-pred_dataframe.index = pd.to_datetime(pred_dataframe.index)
-target_dataset = pd.read_csv(path_input+target_file, index_col=0)
-target_dataset.index = pd.to_datetime(target_dataset.index)
-
+"""
+File names to store the solutions provided by the algorithm
+"""
+experiment_filename = predictor_file[-37:]
+outuput_dir = os.path.join(fs_dir, 'results', 'test')
+os.makedirs(outuput_dir, exist_ok=True)
+indiv_path = os.path.join(outuput_dir, experiment_filename)
+model_kind = 'LogReg'
+solution_filename = 'CRO_' + model_kind + '_' + experiment_filename
 
 # Create an empty file to store the solutions provided by the algorithm
-sol_data = pd.DataFrame(columns=['CV','Test','Sol'])
-
-indiv_file = path_output+filename+'.csv'
-solution_file = 'CRO_LogReg_'+filename+'.csv'
-sol_data.to_csv(indiv_file,sep=' ',header=sol_data.columns,index=None)
-
-
+sol_data = pd.DataFrame(columns=['CV', 'Test', 'Sol'])
+sol_data.to_csv(indiv_path, sep=' ', header=sol_data.columns, index=None)
 
 # Split the dataset into train and test
-first_train = 1951
-last_train = 2010
-
-train_indices = (target_dataset.index.year >=first_train) & (target_dataset.index.year <=last_train)
-test_indices = (target_dataset.index.year >last_train)
+train_yearF = 2013 # Last year of the training dataset
+train_indices = predictors_df.index.year <= train_yearF
+test_indices = predictors_df.index.year > train_yearF
 
 """
 All the following methods will have to be implemented for the algorithm to work properly
@@ -71,10 +66,15 @@ class ml_prediction(AbsObjectiveFunc):
         self.opt = "min" # it can be "max" or "min"
 
         # We set the limits of the vector (window size, time lags and variable selection)
-        self.sup_lim = np.append(np.append(np.repeat(60, pred_dataframe.shape[1]),np.repeat(180, pred_dataframe.shape[1])),np.repeat(1, pred_dataframe.shape[1]))  # array where each component indicates the maximum value of the component of the vector
-        self.inf_lim = np.append(np.append(np.repeat(1, pred_dataframe.shape[1]),np.repeat(0, pred_dataframe.shape[1])),np.repeat(0, pred_dataframe.shape[1])) # array where each component indicates the minimum value of the component of the vector
+        # self.sup_lim = np.append(np.append(np.repeat(60, pred_dataframe.shape[1]),np.repeat(180, pred_dataframe.shape[1])),np.repeat(1, pred_dataframe.shape[1]))  # array where each component indicates the maximum value of the component of the vector
+        # self.inf_lim = np.append(np.append(np.repeat(1, pred_dataframe.shape[1]),np.repeat(0, pred_dataframe.shape[1])),np.repeat(0, pred_dataframe.shape[1])) # array where each component indicates the minimum value of the component of the vector
         # we call the constructor of the superclass with the size of the vector
         # and wether we want to maximize or minimize the function 
+
+        # We set the limits of the vector (window size, time lags and variable selection)
+        self.sup_lim = np.append(np.append(np.repeat(2, predictors_df.shape[1]), np.repeat(2, predictors_df.shape[1])), np.repeat(1, predictors_df.shape[1]))
+        self.inf_lim = np.append(np.append(np.repeat(1, predictors_df.shape[1]), np.repeat(0, predictors_df.shape[1])), np.repeat(0, predictors_df.shape[1]))
+
         super().__init__(self.size, self.opt, self.sup_lim, self.inf_lim)
     
     """
@@ -83,74 +83,59 @@ class ml_prediction(AbsObjectiveFunc):
     def objective(self, solution):
         # print(solution)
         # Read data
-        sol_file = pd.read_csv(indiv_file,sep=' ',header=0)
+        sol_file = pd.read_csv(indiv_path, sep=' ', header=0)
 
         # Read solution
-        time_sequences = np.append(np.array(solution[:pred_dataframe.shape[1]]).astype(int),1)
-        time_lags = np.append(np.array(solution[pred_dataframe.shape[1]:(2*pred_dataframe.shape[1])]).astype(int),1)
-        variable_selection = np.array(solution[(2*pred_dataframe.shape[1]):]).astype(int)
+        time_sequences = np.append(np.array(solution[:predictors_df.shape[1]]).astype(int), 1)
+        time_lags = np.append(np.array(solution[predictors_df.shape[1]:(2*predictors_df.shape[1])]).astype(int), 1)
+        variable_selection = np.array(solution[(2*predictors_df.shape[1]):]).astype(int)
 
         if sum(variable_selection) == 0:  # If no variables are selected, return a high value
             return 100000
 
-
-
-        # # Create dataset according to solution
-        dataset_opt = target_dataset.copy()
-        for i,col in enumerate(pred_dataframe.columns):
-            if variable_selection[i] == 0 or time_sequences[i] == 0:
+        # Create dataset according to solution
+        dataset_opt = target_df.copy()
+        for c, col in enumerate(predictors_df.columns):
+            if variable_selection[c] == 0 or time_sequences[c] == 0:
                 continue
-            for j in range(time_sequences[i]):
-                dataset_opt[str(col)+'_lag'+str(time_lags[i]+j)] = pred_dataframe[col].shift(time_lags[i]+j)
-
-            
+            for j in range(time_sequences[c]):
+                dataset_opt[str(col) +'_lag'+ str(time_lags[c]+j)] = predictors_df[col].shift(time_lags[c]+j)
+   
         # Split dataset into train and test
-
         train_dataset = dataset_opt[train_indices]
         test_dataset = dataset_opt[test_indices]
         
         # Standardize data
-        Y_column = 'Target' 
-            
+        Y_column = 'tcg' 
         X_train=train_dataset[train_dataset.columns.drop([Y_column]) ]
         Y_train=train_dataset[Y_column]
-
         X_test=test_dataset[test_dataset.columns.drop([Y_column]) ]
         Y_test=test_dataset[Y_column]
             
-        from sklearn import preprocessing
         scaler = preprocessing.StandardScaler()
         X_std_train = scaler.fit(X_train)
-
         X_std_train = scaler.transform(X_train)
         X_std_test = scaler.transform(X_test)
-
-        X_train=pd.DataFrame(X_std_train,columns=X_train.columns,index=X_train.index)
-        X_test=pd.DataFrame(X_std_test,columns=X_test.columns,index=X_test.index)
-
+        X_train=pd.DataFrame(X_std_train, columns=X_train.columns, index=X_train.index)
+        X_test=pd.DataFrame(X_std_test, columns=X_test.columns, index=X_test.index)
 
         # Train model
-        from sklearn.metrics import f1_score, mean_absolute_error
-        from sklearn.linear_model import LogisticRegression
         clf = LogisticRegression()
 
-
         # Apply cross validation
-        from sklearn.model_selection import cross_val_score
-        score = cross_val_score(clf, X_train, Y_train, cv=5,scoring='f1')
+        score = cross_val_score(clf, X_train, Y_train, cv=5, scoring='f1')
         clf.fit(X_train, Y_train)
         Y_pred = clf.predict(X_test)
-        print(score.mean(), f1_score(Y_pred,Y_test))
+        print(score.mean(), f1_score(Y_pred, Y_test))
 
         # Save solution
         sol_file = pd.concat([sol_file, pd.DataFrame({'CV': [score.mean()], 'Test': [f1_score(Y_pred, Y_test)], 'Sol': [solution]})], ignore_index=True)
-        sol_file.to_csv(indiv_file,sep=' ',header=sol_file.columns,index=None)
+        sol_file.to_csv(indiv_path, sep=' ', header=sol_file.columns, index=None)
+        
         return 1/score.mean()
     
-
-    
     """
-    This will be the function used to generate random vectorsfor the initializatio of the algorithm
+    This will be the function used to generate random vectors for the initialization of the algorithm
     """
     def random_solution(self):
         return np.random.choice(self.sup_lim[0], self.size, replace=True)
@@ -168,7 +153,8 @@ class ml_prediction(AbsObjectiveFunc):
         #     new = np.random.choice(pool, len(solution) - len(unique), replace=False)
         #     solution = np.concatenate((unique, new))
         return np.clip(solution, self.inf_lim, self.sup_lim)
-objfunc = ml_prediction(3*pred_dataframe.shape[1])
+
+objfunc = ml_prediction(3*predictors_df.shape[1])
 
 params = {
     "popSize": 100,
@@ -199,7 +185,7 @@ params = {
     # "prob_file": "prob_history_"+filename+".csv",
     # "popul_file": "last_population"+filename+".csv",
     # "history_file": "fit_history_"+filename+".csv",
-    "solution_file": "best_solution_"+filename+".csv",
+    "solution_file": "best_solution_" + experiment_filename,
     # "indiv_file": "indiv_hisotry_"+filename+".csv",
 }
 
@@ -214,5 +200,5 @@ cro_alg = CRO_SL(objfunc, operators, params)
 
 solution, obj_value = cro_alg.optimize()
 
-solution.tofile(path_output+solution_file, sep=',')
+solution.tofile(os.path.join(outuput_dir, solution_filename), sep=',')
 
