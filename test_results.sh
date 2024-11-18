8aba7f3ddceba053f3aef84d9a30f3558b78f241
Filippo Dainelli <dainelli.filippo@gmail.com>
2024-11-18 11:46:16 +0100

    print new results for all test simulations + clean of repo

--- test_results.sh
+++ test_results.sh
@@ -8,6 +8,6 @@ n_vars=(14 28)
 for n_cluster in "${n_clusters[@]}"; do
   for n_var in "${n_vars[@]}"; do
     echo "Number of clusters: $n_cluster, Number of variables: $n_var"
-    python3 results_analysis.py --n_clusters $n_cluster --n_vars $n_var --n_idxs 10 --results_folder "test_nc${n_cluster}_nv${n_var}_nd10"
+    python3 test_results_analysis.py --n_clusters $n_cluster --n_vars $n_var --n_idxs 10 --results_folder "test_nc${n_cluster}_nv${n_var}_nd10"
   done
 done
