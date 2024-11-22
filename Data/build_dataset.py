e31388b2bfb663041e9d5800d85679afa1f7daf1
Filippo Dainelli <dainelli.filippo@gmail.com>
2024-11-22 17:35:28 +0100

    fixed dataset construction for sub-basins

--- Data/build_dataset.py
+++ Data/build_dataset.py
@@ -2,14 +2,17 @@ import os
 import argparse
 from utils_dataset import build_dataset
 
-def main(basin, n_clusters, res, first_year, last_year):
+def main(basin, n_clusters, anomaly_clustering, res, first_year, last_year):
     # List of variables to include for the feature selection process
     cluster_variables = ['abs_vo850', 'mpi', 'msl', 'r700', 'sst', 'vo850', 'vws850-200', 'w']
     climate_indexes = ['EA-WR', 'ENSO3.4', 'EP-NP', 'NAO', 'PDO', 'PNA', 'SOI', 'TNA', 'TSA', 'WP']
 
     # Directories to consider to build the dataset for feature selection
     project_dir = '/home/simul6/Documentos/Filippo/'
-    cluster_path = os.path.join(project_dir, 'FS_TCG', 'data', f'{basin}_{n_clusters}clusters')
+    if anomaly_clustering == 'y':
+        cluster_path = os.path.join(project_dir, 'FS_TCG', 'data', f'{basin}_{n_clusters}clusters_anomaly')
+    else:
+        cluster_path = os.path.join(project_dir, 'FS_TCG', 'data', f'{basin}_{n_clusters}clusters')
     indexes_path = os.path.join(project_dir, 'data', 'CI')
     resolution = '{}x{}'.format(res, res)
     target_path = os.path.join(project_dir, 'data', 'IBTrACS', resolution, 'TCG', f'TCG_{resolution}')
@@ -17,7 +20,7 @@ def main(basin, n_clusters, res, first_year, last_year):
     save_path = cluster_path
 
     # Build the dataset and save it to file
-    dataset, target = build_dataset(cluster_variables, climate_indexes, cluster_path, indexes_path, target_path, first_year, last_year, month_col=True)
+    dataset, target = build_dataset(basin, cluster_variables, climate_indexes, cluster_path, indexes_path, target_path, first_year, last_year, month_col=True)
     dataset_filename = f'predictors_{first_year}-{last_year}_{n_clusters}clusters_{len(cluster_variables)}vars_{len(climate_indexes)}idxs.csv'
     dataset.to_csv(os.path.join(save_path, dataset_filename))
     target_filename = f'target_{first_year}-{last_year}_2.5x2.5.csv'
@@ -28,6 +31,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Build dataset for feature selection')
     parser.add_argument('--basin', type=str, default='GLB', help='Basin')
     parser.add_argument('--n_clusters', type=int, default=8, help='Number of clusters')
+    parser.add_argument('--anomaly_clustering', type=str, default='y', help='If y retrieve dataset of anomaly clustering')
     parser.add_argument('--res', type=float, default=2.5, help='Resolution')
     parser.add_argument('--first_year', type=int, default=1965, help='First year')
     parser.add_argument('--last_year', type=int, default=2022, help='Last year')
--- Data/utils_dataset.py
+++ Data/utils_dataset.py
@@ -3,14 +3,62 @@ import pandas as pd
 import xarray as xr
 import os
 
+def crop_field(var, lon1, lon2, lat1, lat2):
+    """
+    Crop the specified variable to the specified domain.
+
+    Inputs:
+        var: xarray.DataArray or xarray.Dataset
+            The variable to crop
+        lon1: float
+            The western longitude of the domain (range: -180 to 180)
+        lon2: float
+            The eastern longitude of the domain (range: -180 to 180)
+        lat1: float
+            The southern latitude of the domain
+        lat2: float
+            The northern latitude of the domain
+    Outputs:
+        ds: xarray.DataArray or xarray.Dataset
+            The cropped variable
+    """
+    ds = var.copy()
+    # If the domain crosses/touches the meridian 180, convert to 0-360
+    if (lon1 <= 180 and lon2 >= -180 and lon1 > lon2) or (lon1 == -180) or (lon2 == 180):
+        ds.coords['longitude'] = (ds.longitude + 360) %360
+        ds = ds.sortby(ds.longitude)
+        if lon2 < 0:
+            lon2 = lon2 + 360
+        if lon1 < 0:
+            lon1 = lon1 + 360
+    return ds.sel(longitude=slice(lon1, lon2), latitude=slice(lat2, lat1))
+
 def check_consecutive_repeats(df,col):
     repeats = df.shift(1) == df
     if repeats.sum() > 0:
         print('Consecutive values repeated found at',col)
         print(repeats[repeats].index)
 
-def build_dataset(cluster_variables, index_variables, cluster_path, indexes_path, target_path, first_year, last_year, month_col=True):
+def build_dataset(basin, cluster_variables, index_variables, cluster_path, indexes_path, target_path, first_year, last_year, month_col=True):
     
+    # Define geographical coordinates according to the basin considered
+    if basin == 'NWP':
+        min_lon, max_lon, min_lat, max_lat = 100, 180, 0, 40
+    elif basin == 'NEP':
+        min_lon, max_lon, min_lat, max_lat = -180, -75, 0, 40
+    elif basin == 'NA':
+        min_lon, max_lon, min_lat, max_lat = -100, 0, 0, 40
+    elif basin == 'NI':
+        min_lon, max_lon, min_lat, max_lat = 45, 100, 0, 40
+    elif basin == 'SP':
+        min_lon, max_lon, min_lat, max_lat = 135, -70, -40, 0
+    elif basin == 'SI':
+        min_lon, max_lon, min_lat, max_lat = 35, 135, -40, 0
+    elif basin == 'GLB':
+        min_lon, max_lon, min_lat, max_lat = -181, 181, -40, 40
+    else:
+        raise ValueError('Basin not recognized')
+
     # Create a dataframe containing the data for the climate indeces
     date_range = pd.date_range(start=f'{first_year}-01-01', end=f'{last_year}-12-01', freq='MS')
     df_indeces = pd.DataFrame(index=date_range, columns=index_variables)
@@ -51,7 +99,15 @@ def build_dataset(cluster_variables, index_variables, cluster_path, indexes_path
 
     # Build the dataframe for the target variable -> number of tropical cyclone genesis events per month
     years = np.arange(first_year, last_year+1, 1)
-    tcg_ds = xr.concat([xr.open_dataset(target_path + f'_{year}.nc') for year in years], dim='time')
+    tcg_ds_or = xr.concat([xr.open_dataset(target_path + f'_{year}.nc') for year in years], dim='time')
+    if basin == 'NEP' or basin == 'NA':
+        tcg_ds = crop_field(tcg_ds_or, min_lon, max_lon, min_lat, max_lat)
+        mask = xr.open_dataarray(f'{basin}_mask.nc')
+        tcg_ds = tcg_ds.where(mask == 1)
+    elif basin != 'GLB':
+        tcg_ds = crop_field(tcg_ds_or, min_lon, max_lon, min_lat, max_lat)
+    else:
+        tcg_ds = tcg_ds_or
     target = pd.DataFrame(index=date_range)
     target['tcg'] = tcg_ds.tcg.sum(dim=['latitude', 'longitude']).values.astype(int)
 
