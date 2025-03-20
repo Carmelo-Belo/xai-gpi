05af491509cfdd05c3470911036d49dd99ff1933
Filippo Dainelli <dainelli.filippo@gmail.com>
2025-03-20 18:45:24 +0100

    Create run_investigation.sh

---
+++ best_model_analysis_noTS/run_investigation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Cycle through the different percentage of selection
+basin="NEP"
+n_clusters=9
+sel_percs=("50" "60" "70" "75" "80" "90")
+# sel_percs=("70" "80")
+
+for perc in "${sel_percs[@]}"; do
+    run_name="selfeat${perc}_top20_nc${n_clusters}_nv8_nd9_noTS"
+    echo "Running run investigation for $basin, $perc% selection"
+    python3 run_investigation.py --basin $basin --run_name $run_name
+done
