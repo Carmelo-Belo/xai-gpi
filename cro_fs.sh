a59039c3486445bf97efa6b2cb156eb1a0a5997d
Filippo Dainelli <dainelli.filippo@gmail.com>
2025-09-24 11:35:40 +0200

    Update cro_fs.sh

--- cro_fs.sh
+++ cro_fs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Cycle through the different number of clusters and different possible models for FS fitness function
-n_clusters=(10 11 12)
-models=('linreg' 'lgbm')
+n_clusters=(5 6 7 8 9 10 11 12)
+models=('linreg' 'lgbm' 'pi-lgbm')
 n_vars=8
 n_idxs=9
-basins=("GLB" "NA" "NEP" "NWP" "NI" "SI" "SP")
+basins=("NA" "NEP" "NWP" "NI" "SI" "SP")
 
 for basin in "${basins[@]}"; do
     for n_cluster in "${n_clusters[@]}"; do
@@ -19,16 +19,12 @@ for basin in "${basins[@]}"; do
                     echo "The folder exists for $basin, $n_cluster normal cluster, $model model, and test $i"
                 else
                     echo "Running FS for $basin with $n_cluster normal clusters, $model model, and test $i"
-                    python3 CRO_Spatiotemporal_FS.py --basin $basin --anomaly_clustering 'n' --n_clusters $n_cluster --n_vars $n_vars --n_idxs $n_idxs --model_kind $model --output_folder $output_folder
-                fi
-                # Simulation with anomaly clusterization
-                output_folder="test${i}_${model}_Anc${n_cluster}_nv${n_vars}_nd${n_idxs}"
-                folder="results/${basin}/${output_folder}"
-                if [ -d $folder ]; then
-                    echo "The folder exists for $basin, $n_cluster anomaly cluster, $model model, and test $i"
-                else
-                    echo "Running FS for $basin with $n_cluster anomaly clusters, $model model, and test $i"
-                    python3 CRO_Spatiotemporal_FS.py --basin $basin --anomaly_clustering 'y' --n_clusters $n_cluster --n_vars $n_vars --n_idxs $n_idxs --model_kind $model --output_folder $output_folder
+                    # run different scripts depending on the model used for the fitnesse function
+                    if [ "$model" == "pi-lgbm" ]; then
+                        python3 CRO_Spatiotemporal_FS_PI.py --basin $basin --n_clusters $n_cluster --remove_trend 'n' --remove_seasonality 'n' --n_vars $n_vars --n_idxs $n_idxs --model_kind $model --output_folder $output_folder
+                    else
+                        python3 CRO_Spatiotemporal_FS.py --basin $basin --n_clusters $n_cluster --remove_trend 'n' --remove_seasonality 'n' --n_vars $n_vars --n_idxs $n_idxs --model_kind $model --output_folder $output_folder
+                    fi
                 fi
             done
         done
