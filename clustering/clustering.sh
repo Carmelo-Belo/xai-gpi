a518f2c1db195804b8a1aaa823e41df4a8eb9772
Filippo Dainelli <dainelli.filippo@gmail.com>
2025-09-25 14:37:56 +0200

    Update clustering.sh

--- clustering/clustering.sh
+++ clustering/clustering.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 # Cycle through the different basins and number of clusters
-basins=("GLB" "NEP" "NWP" "NA" "NI" "SP" "SI")
+basins=("NA" "NEP" "NWP" "NI" "SI" "SP")
 n_clusters=(5 6 7 8 9 10 11 12)
 
 for basin in "${basins[@]}"; do
     for n_cluster in "${n_clusters[@]}"; do
-        echo "Clustering for $basin with $n_cluster clusters"
-        python3 clustering.py --n_clusters $n_cluster --basin $basin --anomaly_clustering 'n' --remove_seasonality 'n'
-        echo "Clustering for $basin with $n_cluster anomaly clusters"
-        python3 clustering.py --n_clusters $n_cluster --basin $basin --anomaly_clustering 'y' --remove_seasonality 'n'
-        echo "Clustering for $basin with $n_cluster deseasonalized clusters"
-        python3 clustering.py --n_clusters $n_cluster --basin $basin --anomaly_clustering 'n' --remove_seasonality 'y'
+        # Normal Clustering
+        python3 clustering.py --n_clusters $n_cluster --basin $basin
+        echo "Normal Clustering for $basin with $n_cluster clusters is done"
+        # Deseasonalized Clustering
+        python3 clustering.py --n_clusters $n_cluster --basin $basin --remove_seasonality 'y'
+        echo "Deseasonalized Clustering for $basin with $n_cluster clusters is done"
+        # Detrended Clustering
+        python3 clustering.py --n_clusters $n_cluster --basin $basin --remove_trend 'y'
+        echo "Detrended Clustering for $basin with $n_cluster clusters is done"
+        # Detrended and Deseasonalized Clustering
+        python3 clustering_noTS.py --n_clusters $n_cluster --basin $basin
+        echo "Detrended and deseasonalized Clustering for $basin with $n_cluster clusters is done"
     done
 done
